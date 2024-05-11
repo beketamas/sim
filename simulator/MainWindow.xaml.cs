fe7d5fcc1d4fad42723a4f4dd33fe0658497183b
beketamas <72067154+beketamas@users.noreply.github.com>
2024-05-11 12:13:58 +0200

    Add files via upload

--- simulator/MainWindow.xaml.cs
+++ simulator/MainWindow.xaml.cs
@@ -4,7 +4,9 @@ using System.Data.Common;
 using System.Linq;
 using System.Numerics;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -27,12 +29,14 @@ namespace simulator
         const int OSZLOPOK_SZAMA = 15;
         const int JATEKOSOK_SZAMA = 30;
         const int AKNAK_SZAM = 10;
-        string[,] matrix = new string[SOROK_SZAMA,OSZLOPOK_SZAMA];
-        List<string> helyek = new List<string>();
+        static string[,] matrix = new string[SOROK_SZAMA,OSZLOPOK_SZAMA];
+        public static List<string> helyek = new List<string>();
+        private DispatcherTimer timer;
         public MainWindow()
         {
             InitializeComponent();
 
+            sldCsuszka.Value = 250;
             tabla.ColumnDefinitions.Clear();
             tabla.RowDefinitions.Clear();
             tabla.Background = new SolidColorBrush(Colors.LightGreen);
@@ -113,59 +117,88 @@ namespace simulator
                 }
             }
 
-            btnLepes.Click += (s, r) =>
+            btnLepes.Click += (s, e) => Lepkedes(tabla);
+            auto.Checked += (s, e) =>
             {
-                foreach (var item in tabla.Children)
+                btnLepes.IsEnabled = false;
+                timer = new()
                 {
-                    if (item is Button)
-                    {
-                        int sor = Grid.GetRow(item as Button);
-                        int oszlop = Grid.GetColumn(item as Button);
-                        List<string> listLehetosegek = new List<string>();
-                        if (sor + 1 < SOROK_SZAMA && matrix[sor + 1, oszlop] == "semmi")
-                            listLehetosegek.Add("le");
-                        if (sor - 1 > 0 && matrix[sor - 1, oszlop] == "semmi")
-                            listLehetosegek.Add("fel");
-                        if (oszlop + 1 < OSZLOPOK_SZAMA && matrix[sor, oszlop + 1] == "semmi")
-                            listLehetosegek.Add("jobb");
-                        if (oszlop - 1 >= 0 && matrix[sor, oszlop - 1] == "semmi")
-                            listLehetosegek.Add("bal");
-                        Random numLep = new();
-                        if (listLehetosegek.Count != 0)
-                        {
-                            switch (listLehetosegek[numLep.Next(0, listLehetosegek.Count)])
-                            {
-                                case "le":
-                                    Grid.SetColumn(item as Button, Grid.GetColumn(item as Button));
-                                    Grid.SetRow(item as Button, Grid.GetRow(item as Button) + 1);
-                                    matrix[sor, oszlop] = "semmi";
-                                    matrix[sor + 1, oszlop] = "jatekos";
-                                    break;
-                                case "fel":
-                                    Grid.SetColumn(item as Button, Grid.GetColumn(item as Button));
-                                    Grid.SetRow(item as Button, Grid.GetRow(item as Button) - 1);
-                                    matrix[sor, oszlop] = "semmi";
-                                    matrix[sor - 1, oszlop] = "jatekos";
-                                    break;
-                                case "jobb":
-                                    Grid.SetColumn(item as Button, Grid.GetColumn(item as Button) + 1);
-                                    Grid.SetRow(item as Button, Grid.GetRow(item as Button));
-                                    matrix[sor, oszlop] = "semmi";
-                                    matrix[sor, oszlop + 1] = "jatekos";
-                                    break;
-                                case "bal":
-                                    Grid.SetColumn(item as Button, Grid.GetColumn(item as Button) - 1);
-                                    Grid.SetRow(item as Button, Grid.GetRow(item as Button));
-                                    matrix[sor, oszlop] = "semmi";
-                                    matrix[sor, oszlop - 1] = "jatekos";
-                                    break;
-                            }
-                        }
-                        Random num = new Random();
-                        (item as Button).Background = new SolidColorBrush(Color.FromRgb(Convert.ToByte(num.Next(0,255)), Convert.ToByte(num.Next(0, 255)), Convert.ToByte(num.Next(0, 255))));
-                    }
-                }
+                    Interval = TimeSpan.FromMilliseconds(sldCsuszka.Value)
+                };
+                timer.Tick += Timer_Tick;
+                sldCsuszka.ValueChanged += Slider_ValueChanged;
+                timer.Start();
             };
+            manual.Checked += (s, e) => btnLepes.IsEnabled = true;
+        }
+        public static void VaneEAkna(int x, int y, List<string> lista, string merre) => lista.Add(matrix[x, y] == "akna" ? "akna" : merre);
+        private void Timer_Tick(object sender, EventArgs e) => Lepkedes(tabla);
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            timer.Interval = TimeSpan.FromMilliseconds(sldCsuszka.Value);
+            timer.Stop();
+            timer.Start();
+        }
+        public static void Lepkedes(Grid tabla)
+        {
+            for (int i = 0; i < tabla.Children.Count; i++)
+            {
+                if (tabla.Children[i] is Button)
+                {
+                    Button? gomb = tabla.Children[i] as Button;
+                    int sor = Grid.GetRow(gomb);
+                    int oszlop = Grid.GetColumn(gomb);
+                    List<string> listLehetosegek = new List<string>();
+                    if (sor + 1 < SOROK_SZAMA && (matrix[sor + 1, oszlop] == "semmi" || matrix[sor + 1, oszlop] == "akna"))
+                        VaneEAkna(sor + 1, oszlop, listLehetosegek, "le");
+                    if (sor - 1 > 0 && (matrix[sor - 1, oszlop] == "semmi" || matrix[sor - 1, oszlop] == "akna"))
+                        VaneEAkna(sor - 1, oszlop, listLehetosegek, "fel");
+                    if (oszlop + 1 < OSZLOPOK_SZAMA && (matrix[sor, oszlop + 1] == "semmi" || matrix[sor, oszlop + 1] == "akna"))
+                        VaneEAkna(sor, oszlop + 1, listLehetosegek, "jobb");
+                    if (oszlop - 1 >= 0 && (matrix[sor, oszlop - 1] == "semmi" || matrix[sor, oszlop - 1] == "akna"))
+                        VaneEAkna(sor, oszlop - 1, listLehetosegek, "bal");
+
+                    Random numLep = new();
+                    if (listLehetosegek.Count != 0)
+                    {
+                        switch (listLehetosegek[numLep.Next(0, listLehetosegek.Count)])
+                        {
+                            case "le":
+                                Grid.SetColumn(gomb, Grid.GetColumn(gomb));
+                                Grid.SetRow(gomb, Grid.GetRow(gomb) + 1);
+                                matrix[sor, oszlop] = "semmi";
+                                matrix[sor + 1, oszlop] = "jatekos";
+                                break;
+                            case "fel":
+                                Grid.SetColumn(gomb, Grid.GetColumn(gomb));
+                                Grid.SetRow(gomb, Grid.GetRow(gomb) - 1);
+                                matrix[sor, oszlop] = "semmi";
+                                matrix[sor - 1, oszlop] = "jatekos";
+                                break;
+                            case "jobb":
+                                Grid.SetColumn(gomb, Grid.GetColumn(gomb) + 1);
+                                Grid.SetRow(gomb, Grid.GetRow(gomb));
+                                matrix[sor, oszlop] = "semmi";
+                                matrix[sor, oszlop + 1] = "jatekos";
+                                break;
+                            case "bal":
+                                Grid.SetColumn(gomb, Grid.GetColumn(gomb) - 1);
+                                Grid.SetRow(gomb, Grid.GetRow(gomb));
+                                matrix[sor, oszlop] = "semmi";
+                                matrix[sor, oszlop - 1] = "jatekos";
+                                break;
+                            case "akna":
+                                matrix[sor, oszlop] = "semmi";
+                                UIElement? element = tabla.Children.Cast<UIElement>().FirstOrDefault(e => Grid.GetRow(e) == sor && Grid.GetColumn(e) == oszlop);
+                                tabla.Children.Remove(element);
+                                break;
+                        }
+                    }
+                    Random num = new Random();
+                    gomb.Background = new SolidColorBrush(Color.FromRgb(Convert.ToByte(num.Next(0, 255)), Convert.ToByte(num.Next(0, 255)), Convert.ToByte(num.Next(0, 255))));
+
+                }
+            }
         }
     }
 }
